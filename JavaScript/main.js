616ff9b65ac27b31e98d8f83421e0617f5b04aa0
Ryusei <nhrryi@gmail.com>
2021-03-03 23:41:58 +0900

    make up main page layout

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -5,17 +5,15 @@ let config = {
 }
 
 class User{
-    constructor(username, passward, assets=50000, age=20, days=1) {
+    constructor(username, password, assets=50000, age=20, days=1) {
         this.username = username;
-        this.passward = passward;
+        this.password = password;
         this.assets = assets;
         this.age = age;
         this.days = days;
     }
 
-    static getUserNameAndPass() {
-        const form = document.getElementById("userinfo");
-    }
+
 }
 
 let userlist = [
@@ -24,6 +22,13 @@ let userlist = [
     new User("James", "000", 10000000, 48),
     new User("Curry", "100", 5000000, 35),
 ];
+
+let userManagement = {
+    "Davis" : new User("Davis", "200", 4903000000, 68),
+    "Durant" : new User("Durant", "300", 200000000, 54),
+    "James" : new User("James", "000", 10000000, 48),
+    "Curry" : new User("Curry", "100", 5000000, 35),
+};
 
 
 
@@ -51,20 +56,57 @@ class NavBar{
 
 
 class UserOperation {
+
     static clickYesOrNo(){
         document.getElementById("yes").addEventListener("click", function(){
-            ViewController.displayNone(config.firstPage);
-            config.mainpage.append(ViewController.usertable(userlist));
+            ViewController.displayNone(config.confirm);
+            config.firstPage.append(ViewController.loginpage(1));
         })
 
         document.getElementById("no").addEventListener("click", function(){
-            ViewController.displayNone(config.confirm);
-            config.firstPage.append(ViewController.loginpage(0));
+            ViewController.changeClassEle(null, "bg-dark", config.mainpage);
+            ViewController.displayNone(config.firstPage);
+            config.mainpage.append(ViewController.usertable(userlist));
+            UserOperation.chooseUser();
         })
     }
 
-    static print(flag){
-        console.log(flag);
+    static chooseUser() {
+        let tbodyList = document.querySelectorAll("tbody > tr");
+        let username = "";
+        tbodyList.forEach(element=>element.addEventListener("click", function() {
+            ViewController.changeClassEle(null, "bg-white", config.mainpage);
+            ViewController.displayNone(config.confirm);
+            ViewController.displayNone(document.querySelectorAll(".usertable")[0]);
+            ViewController.displayBlock(config.firstPage);
+
+            username = element.querySelectorAll(".username")[0].innerHTML;
+            config.firstPage.append(ViewController.loginpage(0));
+            config.firstPage.querySelectorAll(`input[name="username"]`)[0].value = username;
+        }))
+        
+    }
+
+
+    static getUserInfo(flag){
+        let form = document.getElementById("userinfo");
+        let username = form.querySelectorAll(`input[name="username"]`).item(0).value;
+        let password = form.querySelectorAll(`input[name="password"]`).item(0).value;
+
+        if (flag) {
+            let user = new User
+            (
+                username,
+                password
+            );
+            userlist.push(user);
+            userManagement[username] = user;
+        }
+        else {
+            UserManagement.checkUser(userManagement, username, password);
+            console.log();
+        }
+
     }
 
 };
@@ -80,16 +122,29 @@ class ViewController {
         ele.classList.remove("d-none");
     }
 
+    static changeClassEle(blotout, input, ele) {
+        ele.classList.remove(blotout);
+        ele.classList.add(input);
+    }
+
     
     static usertable(userlist) {
         let container = document.createElement("div");
-        container.classList.add("container");
+        container.classList.add("container", "usertable");
         let table = document.createElement("table");
         table.classList.add("table", "table-hover");
+
+        container.innerHTML =
+        `
+        <h2 class="text-center text-white">
+            Clicker Empire Game Users
+        </h2>
+        <p class="text-center font-monospace text-white mt-4">Please Choose...</p>
+        `
     
         table.innerHTML = 
         `
-        <thead class="thead-dark">
+        <thead class="thead-dark text-white">
         <tr>
             <th>#</th>
             <th>UserName</th>
@@ -100,14 +155,15 @@ class ViewController {
         `
     
         let tbody = document.createElement("tbody");
+        tbody.classList.add("text-white");
         for (let i = 0; i < userlist.length; i++) {
             tbody.innerHTML += 
             `
-            <tr>
-                <th scope="row" class=${i+1}>${i+1}</th>
-                <td class=${i+1}>${userlist[i].username}</td>
-                <td class=${i+1}>${userlist[i].age}</td>
-                <td class=${i+1}>${new Intl.NumberFormat().format(userlist[i].assets)}</td>
+            <tr class=${i+1}>
+                <th scope="row">${i+1}</th>
+                <td class="username">${userlist[i].username}</td>
+                <td>${userlist[i].age}</td>
+                <td>${new Intl.NumberFormat().format(userlist[i].assets)}</td>
             </tr>
             `
         }
@@ -117,13 +173,13 @@ class ViewController {
     }
 
     static loginpage(flag) {
-        let btnname = flag ? "Register" : "Login";
+        let btnname = flag ? "SignUp" : "Login";
         let container = document.createElement("div");
         container.innerHTML = 
         `
-        <form action="" id="userinfo" onsubmit="UserOperation.print(${flag}); event.preventDefault()">
+        <form action="" id="userinfo" onsubmit="UserOperation.getUserInfo(${flag}); event.preventDefault()">
             <div class="input-group mt-3">
-                <input type="text" placeholder="UserName" class="form-control" name="username">
+                <input type="text" placeholder="UserName" class="form-control" name="username" value="">
             </div>
             <div class="input-group mt-3">
                 <input type="password" placeholder="PassWord" class="form-control" name="password">
@@ -138,17 +194,26 @@ class ViewController {
         `
         return container;
     }
+
     
 }
 
+class UserManagement {
 
+    static checkUser(userManagement, username, password) {
+        let user = userManagement[username];
+        if (user.password != password) {
+            alert("Either the user ID or password is invalid");
+            return false;
+        }
+        else {
+            console.log("OK");
+            return true;
+        }
 
-let userManagement = {
-    "James" : "000",
-    "Durant" : "300",
-    "Davis" : "200",
-    "Curry" : "100",
-}
+    }
+} 
+
 
 
 UserOperation.clickYesOrNo();
